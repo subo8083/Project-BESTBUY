d9dc93918a6cdccf6e379c1a243d31121633f9df
suranjanachary <101392637+suranjanachary@users.noreply.github.com>
2022-07-22 00:22:52 +0530

    2pages

---
+++ PRODUCT PAGES(2)/Scripts/laptops.js
@@ -0,0 +1,48 @@
+import carousal from "./carousal.js";
+
+document.getElementById("carousal").innerHTML=carousal()
+
+const url=`https://suranjanachary.github.io/db/db.json`
+
+let getData=async()=>{
+let res=await fetch(url);
+let data=await res.json();
+appendData(data.products.laptops)
+console.log(data.products)
+}
+getData()
+let appendData=(data)=>{
+data.forEach(function(el){
+
+    let card=document.createElement("div");
+    card.setAttribute("id","card")
+
+    let logo_div= document.createElement("div");
+
+    let logo=document.createElement("img");
+    logo.src=el.logo;
+    logo.setAttribute("class","logos")
+
+    let img=document.createElement("img");
+    img.src=el.image; 
+    img.setAttribute("class","images")
+
+    let desc=document.createElement("h5");
+    desc.innerText=el.desc1;    
+
+    let but=document.createElement("button");
+    but.setAttribute("id","buy");
+    but.innerText="Buy Now"
+    but.addEventListener("click",clickme)
+
+    logo_div.append(logo)
+
+    card.append(logo_div,img,desc,but)
+    document.getElementById("res").append(card)
+
+})
+}
+
+function clickme(){
+window.location.href="Dummy.html"
+}
