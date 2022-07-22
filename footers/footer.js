3a839107a925c8d3101efb6afa5c4a5ec55dabe7
Shivendra Katiyar <17602224@velsuniv.ac.in>
2022-07-23 01:40:39 +0530

    Adding footer files Day-2

---
+++ footers/footer.js
@@ -0,0 +1,65 @@
+function footer(){
+    return`
+    <div id="sectionTop">
+            <div id="support">
+                <h3>Customer Support</h3>
+                <a href="https://www.bestbuy.ca/en-ca/help/your-order/contact-us"><p>Contact Us</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/help"><p>Help Centre</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/help/returns-and-exchanges"><p>Returns & Exchanges</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/about/best-buy-financing/blt12a5f527f63859b7"><p>Best Buy Financing</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/about/gift-cards/blta5578c9ddd209cd8"><p>Best Buy Gift Card</p></a>
+            </div>
+            <div id="account">
+                <h3>Account</h3>
+                <a href="https://www.bestbuy.ca/order/en-ca"><p>Order Status</p></a>
+                <a href="https://www.bestbuy.ca/account/en-ca"><p>Manage Account</p></a>
+                <a href="https://www.bestbuy.ca/account/en-ca/email-preferences"><p>Email Preferences</p></a>
+            </div>
+            <div id="services">
+                <h3>Services</h3>
+                <a href="https://www.bestbuy.ca/en-ca/services/geek-squad-services/bltb5f906bfb57d7744"><p>Geek Squar</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/services/in-home-advisor/blt56f8fe443ee68778"><p>In-Home Advisor</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/event/trade-in-program/blt339c714a25ebe963"><p>Trade-In Program</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/help/haul-away-recycling-and-recalls/battery-and-electronic-recycling-program"><p>Electronics recycling</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/services/best-buy-health/blt4265f1a522b6ec4e"><p>Best Buy Health</p></a>
+            </div>
+            <div id="subscribe">
+                <h3>Be The First To Know</h3>
+                <p>Sign up to stay in the loop about the hottest deals, coolest new products, and exclusive sales events.</p>
+                <input type="email" id="email" placeholder="Enter Email Address">
+                <button id="submit">Submit</button>
+                <div id="socialHandle">
+                    <div><a href="https://www.facebook.com/BestBuyCanada"><img src="https://cdn-icons-png.flaticon.com/128/174/174848.png" alt="facebookHandle"></a></div>
+                    <div><a href="https://www.instagram.com/bestbuycanada"><img src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" alt="InstagramHandle"></a></div>
+                    <div><a href="https://www.linkedin.com/company/best-buy"><img src="https://cdn-icons.flaticon.com/png/128/3536/premium/3536505.png?token=exp=1658337504~hmac=49cd99413e6e3215627230629af76100" alt="linkedinHandle"></a></div>
+                    <div><a href="https://www.pinterest.com/bestbuycanada/"><img src="https://cdn-icons.flaticon.com/png/128/3536/premium/3536559.png?token=exp=1658337530~hmac=af349e3ac050adfd300039a00d0a756e" alt="pinterestHandle"></a></div>
+                    <div><a href="https://www.twitter.com/bestbuycanada"><img src="https://cdn-icons.flaticon.com/png/128/3256/premium/3256013.png?token=exp=1658337548~hmac=59d821718741b21e0250943706abe41b" alt="twitterHandle"></a></div>
+                    <div><a href="https://www.youtube.com/user/CanadaBestBuy"><img src="https://cdn-icons.flaticon.com/png/128/3938/premium/3938026.png?token=exp=1658337568~hmac=b6f9a742cd9d143246bd04b6459f23c1" alt="youtubeHandle"></a></div>
+                </div>
+            </div>
+            
+        </div>
+        <div id="sectionBottom">
+            <div id="aboutUs">
+                <h3>About Us</h3>
+                <a href="https://www.bestbuy.ca/en-ca/careers/"><p>Careers</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/about/about-us/blt6d2c74aaa5bd3e78"><p>Corporate Information</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/about/what-is-marketplace/blt0abb2e20522802f2"><p>About Best Buy MarketPlace</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/about/selling-on-marketplace/blt38484947eacf725e"><p>Sell on Best Buy Marketplace</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/about/affiliate-program/blt82df225e80ec75e9"><p>Best Buy Affiliate Program</p></a>
+                <a href="https://www.bestbuy.ca/en-ca/about/best-buy-advertising/blteb783fe55d4099b4"><p>Advertise with Best Buy</p></a>
+            </div>
+            <div id="international">
+                <h3>International Sites</h3>
+                <a href="https://www.bestbuy.com/?intl=nosplash&_ga=2.79501204.1634394815.1658229060-1258553307.1658229059"><p>Best Buy US</p></a>
+                <a href="https://www.bestbuy.com/?intl=nosplash&_ga=2.79501204.1634394815.1658229060-1258553307.1658229059"><p>Best Buy Canada</p></a>
+            </div>
+            <div id="mobileApps">
+                <h3>Mobile Apps</h3>
+                <a href="https://play.google.com/store/apps/details?id=com.coppi.bestbuy&hl=en-CA"><p><img src="https://cdn-icons-png.flaticon.com/128/38/38002.png" alt="Android" /> Android App</p></a>
+                <a href="https://itunes.apple.com/ca/app/best-buy-canada/id386960831?l=en-CA"><p><img src="https://cdn-icons-png.flaticon.com/128/731/731985.png" alt="IOS" /> IOS App</p></a>
+            </div>
+        </div>`
+}
+
+export default footer;
